31af43fec2dadbcfd804235eec76f20809e9ba09
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-26 01:51:33 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import argparse
 def main():
     parser = argparse.ArgumentParser(description='Description of your script')
-    parser.add_argument('--optional_arg', type=int, default=0, help='Description of an optional argument')
+    parser.add_argument('input-filename', type=str, default="", help='Input midi file.')
 
     args = parser.parse_args()
 
     # Access the arguments
-    print(f'Optional Argument: {args.optional_arg}')
+    print(f'Arguments: {args}')
     
     print("Hello, World!")
 
