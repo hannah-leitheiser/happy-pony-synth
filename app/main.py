1e0d0b17969d98692f64d7e1d8c31de0f6f88cb5
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-27 07:39:01 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import mido
 
 def main():
     parser = argparse.ArgumentParser(description='Description of your script')
-    parser.add_argument('input-filename', type=str, default="", help='Input midi file.')
+    parser.add_argument('midifilename', type=str, default="", help='Input midi file.')
 
     args = parser.parse_args()
 
     # Access the arguments
     print(f'Arguments: {args}')
 
-    songname = args["input-filename"]
+    songname = args.midifilename
     mid = mido.MidiFile(songname)
     for msg in mid:
         print(msg)
